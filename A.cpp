80f5dd12accd4f760ad442e47a82cb3416c12fbc
Shin <stariver0812@gmail.com>
2020-08-30 12:11:08 +0900

    modified code

--- A.cpp
+++ A.cpp
@@ -4,6 +4,8 @@ using namespace std;
 #define int long long
 
 signed main() {
-    int n;
-    cin >> n;
+    int d, t, s;
+    cin >> d >> t >> s;
+    cout << (1.0 * d / s <= t ? "Yes" : "No") << endl;
+    return 0;
 }
